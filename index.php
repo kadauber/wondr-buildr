bef902729dc0084b9222b9cbacecd9a61fa21d0d
Kimberly A Dauber <kadauber@mit.edu>
2016-06-13 15:10:52 -0400

    initialized preliminary table viewing

--- index.php
+++ index.php
@@ -7,22 +7,32 @@
     <h1>Wondr Buildr</h1>
     <p>Welcome to Wondr Buildr. Go build some wonders.</p>
     <?php
-        echo "You might even be able to see your wonders here someday, if you try hard and believe in yourself."
-//        $server = "sql.mit.edu";
-/*
+        echo "<p>You might even be able to see your wonders here someday, if I try hard and believe in myself.</p>";
+        
         $servername = "sql.mit.edu";
         $username = "kadauber";
         $password = "0012MOAIKate";
 
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=/'Wonders/'", $username, $password);
+            $conn = new PDO("mysql:host=$servername;dbname=kadauber+Wonders", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected to the Wonders database.";
+            echo "<p>Connected to the Wonders database.</p>";
+
+            $statement = $conn->prepare("select * from Wonders");
+            $statement->execute();
+            $statement->setFetchMode(PDO::FETCH_NUM);
+            $result = $statement->fetchAll();
+            foreach ($result as $row) { 
+                echo "<p>";
+                foreach ($row as $field) {
+                    echo $field . " ";
+                }
+                echo "</p>";
+            }
         } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
+            echo "Error: " . $e->getMessage();
         }
-*/
     ?>
 </body>
 </html>
