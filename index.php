0a718ee333705d065504ab3450ca8fd217bf947c
Kimberly A Dauber <kadauber@mit.edu>
2016-06-13 11:02:25 -0400

    changed index from html to php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wondr Buildr</title>
+</head>
+<body style="font-family:Courier New">
+    <h1>Wondr Buildr</h1>
+    <p>Welcome to Wondr Buildr. Go build some wonders.</p>
+    <?php
+        echo "You might even be able to see your wonders here someday, if you try hard and believe in yourself."
+//        $server = "sql.mit.edu";
+/*
+        $servername = "sql.mit.edu";
+        $username = "kadauber";
+        $password = "0012MOAIKate";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=/'Wonders/'", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected to the Wonders database.";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+*/
+    ?>
+</body>
+</html>
