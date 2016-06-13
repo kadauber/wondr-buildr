3105486165c98b3ed67f84b09ed2f7894b4b5d07
Kimberly A Dauber <kadauber@mit.edu>
2016-06-13 17:30:09 -0400

    slight aesthetic modifications

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Wondr Buildr</h1>
     <p>Welcome to Wondr Buildr. Go build some wonders.</p>
     <?php
-        echo "<p>You might even be able to see your wonders here someday, if I try hard and believe in myself.</p>";
+        echo "<p>You might even be able to see some wonders here someday, if I try hard and believe in myself.</p>";
         
         $servername = "sql.mit.edu";
         $username = "kadauber";
@@ -17,7 +17,10 @@
             $conn = new PDO("mysql:host=$servername;dbname=kadauber+Wonders", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "<p>Connected to the Wonders database.</p>";
+            echo "<br><p>Connected to the Wonders database.</p>";
+
+            echo "<br>";
+            echo "<h2><em>Wonders</em></h2>";
 
             $statement = $conn->prepare("select * from Wonders");
             $statement->execute();
@@ -33,6 +36,9 @@
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
+
     ?>
+    <br>
+    <p>And maybe, just maybe, you'll be able to add your own wonders to the list. You never know.</p>
 </body>
 </html>
