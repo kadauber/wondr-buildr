206a501b8d87d80f9359bd1d0fb7dcc4cc6ae410
Loren Sherman <lsherman@fb.com>
2016-06-15 09:46:08 -0400

    Added (non-functional) buttons to delete and edit wonders, plus Create button

--- index.php
+++ index.php
@@ -56,6 +56,10 @@
             foreach ($row as $field) {
                 echo "<td>" . dotParse($field) . "</td>";
             }
+            echo "<td style='text-align:center;'>
+              <a class='textDelete' onclick='deleteWonder()'>&#10005</a>
+              <a class='textEdit' onclick='editWonder()'>&#9998</a>
+              </td>";
             echo "</tr>";
         }
 
@@ -73,6 +77,12 @@
         }
 
     ?>
+
+    <script>
+      //Placeholder
+      function deleteWonder() {window.alert("Wonder Deleted");}
+      function deleteWonder() {window.alert("Wonder Edited");}
+    </script>
 
     <form action="submit_wonder.php" method="post"><tr class="entry">
 	<td><input type="text" name="name" placeholder="Name"></td>
@@ -98,10 +108,12 @@
 		<option value="5">5</option>
 	    </select>
 	</td>
+  <td>
+    <input type="submit" value="Create"/>
+  </td>
   </tr></form>
   </table>
   <br>
-  <p>Use 'Enter' to submit a wonder.</p>
 </div>
 </div>
 </body>
